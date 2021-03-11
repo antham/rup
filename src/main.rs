7c3f4744ab27022041540b14558d9d35cc97194b
Anthony HAMON <hamon.anth@gmail.com>
2021-03-11 21:24:21 +0100

    src : add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    App::new("rup")
+        .version("0.0.1")
+        .author("Anthony Hamon <hamon.anth@gmail.com>")
+        .about("jq for html, fork of ericchiang/pup")
+        .arg(Arg::with_name("selector").index(1))
+        .get_matches();
 }
